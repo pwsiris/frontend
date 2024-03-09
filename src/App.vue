25867e964b50a61b95d59545d6d47b71de923bf0
mirakzen <mirakzen@gmail.com>
2024-03-09 23:21:44 +0300

    small menu fix

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,7 @@
                                 :key="link.url"
                                 :to="link.url"
                                 class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7"
+                                @click="mobileMenuOpen = false"
                             >{{ link.name }}</router-link>
                         </div>
                     </div>
