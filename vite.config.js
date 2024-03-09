cc0bce4243b49f36070677cf4df5bb13e2d11398
mirakzen <mirakzen@gmail.com>
2024-03-10 01:28:47 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,13 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    includeAbsolute: false
+                }
+            }
+        }),
         vueJsx(),
     ],
     resolve: {
